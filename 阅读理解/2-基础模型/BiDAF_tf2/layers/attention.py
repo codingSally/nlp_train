b07b4d55c19db273a194184565d81624fb4a69dd
薛尚岭 <xueshangling@58.com>
2020-11-13 18:07:31 +0800

    提交BiDaf知识梳理

---
+++ 阅读理解/2-基础模型/BiDAF_tf2/layers/attention.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+class C2QAttention(tf.keras.layers.Layer):
+
+    def call(self, similarity, qencode):
+        # homework 
+
+        return c2q_att
+
+class Q2CAttention(tf.keras.layers.Layer):
+
+    def call(self, similarity, cencode):
+        # homework 
+
+        return q2c_att
